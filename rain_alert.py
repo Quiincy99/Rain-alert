35d6b5acc69734c6a48ec47ac4f672a90b54f635
Quiincy99 <lam159sss@gmail.com>
2022-02-24 02:52:19 +0700

    commit

---
+++ rain_alert.py
@@ -0,0 +1,43 @@
+from email import message
+from http import client
+from urllib import response
+import requests
+from twilio.rest import Client
+
+
+LAT = 29.716620
+LNG = -107.808810
+
+OWM_API_KEY = "950844bf0c2ae88923aafee8f0d54e43"
+OWM_URL = "https://api.openweathermap.org/data/2.5/onecall"
+
+account_sid = "AC1b41dd98623c79e0a2c1784ead1c8d4c"
+auth_token = "3d3bfdc86746817ac864cb496473cbad"
+
+weather_params = {
+    "lat": LAT,
+    "lon": LNG,
+    "appid": OWM_API_KEY,
+    "exclude": "current, minutely, daily"
+}
+
+response  = requests.get(OWM_URL, params= weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain",
+            from_= "+16205778341",
+            to= "+87379925451"
+        )
